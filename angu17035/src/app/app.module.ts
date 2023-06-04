29d5bae69ac6461a4a3d8562cbd7e987906589bf
ducnam2003 <namndph24016@fpt.edu.vn>
2023-06-04 17:46:14 +0700

    up trang admin

--- angu17035/src/app/app.module.ts
+++ angu17035/src/app/app.module.ts
@@ -9,13 +9,14 @@ import { HomeAdminComponent } from './pages/admin/home-admin/home-admin.componen
 import { NotPageComponent } from './pages/not-page/not-page.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
-<<<<<<< HEAD
+
 import { SignupComponent } from './pages/signup/signup.component';
 import { SigninComponent } from './pages/signin/signin.component';
 import { CProductComponent } from './pages/c-product/c-product.component';
-=======
-import { ProductComponent } from './pages/product/product/product.component';
->>>>>>> 923b4557f9c472f828cc14e19810e6bbb11b0f03
+import { AddProductComponent } from './pages/admin/add-product/add-product.component';
+import { TrangchuAdminComponent } from './pages/admin/trangchu-admin/trangchu-admin.component';
+
+
 
 
 
@@ -27,13 +28,14 @@ import { ProductComponent } from './pages/product/product/product.component';
     NotPageComponent,
     FooterComponent,
     HeaderComponent,
-<<<<<<< HEAD
+
     SignupComponent,
     SigninComponent,
-    CProductComponent
-=======
-    ProductComponent
->>>>>>> 923b4557f9c472f828cc14e19810e6bbb11b0f03
+    CProductComponent,
+    AddProductComponent,
+    TrangchuAdminComponent
+
+
   ],
   imports: [
     IonicModule.forRoot(),
